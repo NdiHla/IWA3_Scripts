e19e4f9357b93dee2383bb867f37a2feeb4c3c68
Ndimande <125868614+NdiHla@users.noreply.github.com>
2023-04-03 23:43:08 +0200

    ScriptsChallanges1&2

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// configuration.js
+
+const company = 'ACME Inc.'
+const year = 2022
+
+// scripts.js 
+
+'import company form' ('configuration')
+'import year form' ('configuration')
+
+const message = '© ' + company + ' (' + year + ')'
+document.querySelector('footer').innerText = message
+
+// scripts.js
+
+console.log('Roles:', nwabisa.role, johanes.role, alex.role)
